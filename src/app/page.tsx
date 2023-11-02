172f2a289a9a1efc199f13f270d02bdb0b4879b3
Ricardo silva <ricardos8977@gmail.com>
2023-11-02 15:39:27 +0000

    feat: palette

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
 import Environment from './components/environment/Environment'
 import Header from './components/header/Header'
 import Hero from './components/hero/Hero'
-import WrittenText from './components/written-text/WrittenText'
 import styles from './page.module.scss'
 
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      {/* <Environment></Environment> */}
+      <Environment></Environment>
       <Header></Header>
       <Hero></Hero>
       {/* <About></About>
